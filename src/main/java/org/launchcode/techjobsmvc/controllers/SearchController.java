c9d0ad5990d694d77c0205d7cf45cbcc891a2a1f
Edward King <king8three@gmailcom>
2024-10-26 14:35:19 -0400

    Task 4 complete, ready to submit

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -27,6 +27,7 @@ public class SearchController {
         return "search";
     }
 
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping(value = "results")
     public String displaySearchResults(Model model,
