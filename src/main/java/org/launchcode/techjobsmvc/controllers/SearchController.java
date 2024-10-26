9ec9b5ef601083ae17ddc824be9a40147da3beca
Edward King <king8three@gmailcom>
2024-10-26 14:34:23 -0400

    Task 4 complete

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -4,7 +4,10 @@ import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -25,21 +28,20 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
     @PostMapping(value = "results")
-    public String displaySearchResults(Model model, @RequestParam String searchType,@RequestParam String searchTerm){
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if(searchTerm.isEmpty()|| searchType.equals("all")){
-         jobs = JobData.findAll();
+        if (searchTerm.toLowerCase().equals("") || searchTerm.equals("all")) {
+            jobs = JobData.findAll();
         } else {
-            jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-        model.addAttribute("jobs", "jobs");
+        model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
-        model.addAttribute("title", "Jobs With" + searchTerm);
-
+        model.addAttribute("title", "Jobs With " + searchTerm );
         return "search";
 
     }
 }
-
